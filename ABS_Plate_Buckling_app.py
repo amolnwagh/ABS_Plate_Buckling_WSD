3707a3def3e10b4268dbb07087ca168b4d2629d7
amolnwagh <waghamoln02@gmail.com>
2024-07-10 16:39:44 +0530

    Update ABS_Plate_Buckling_app.py

--- ABS_Plate_Buckling_app.py
+++ ABS_Plate_Buckling_app.py
@@ -4,12 +4,6 @@ st.markdown("# ABS Plate Buckling Checks")
 st.markdown("### (WSD Method) - *July 2022 Edition*")
 st.divider()
 
-with st.sidebar:
-    s = st.number_input("Shorter side of panel: s (in)",min_value=0.0001)
-    l = st.number_input("Longer side of panel: l (in)",min_value=s)
-    alpha = s/l
-    st.write(f'Aspect ratio {alpha = }')
-
 st.image(r"figures/typ_stiffened_panel.png","Figure 1: Typical Stiffened Panel")
 st.divider()
 
@@ -19,6 +13,36 @@ with col1:
 
 with col2:
     st.image(r"figures/loads_on_panel.png","Figure 3: Typical Loads on a Stiffened Panel")
-
 st.divider()
 
+with st.sidebar:
+    st.markdown("# Inputs")
+    
+    panel_inputs = st.expander("Plate Panel Dimensions (Refer Figure 1)",expanded=True)
+    with panel_inputs:
+        s = st.number_input("Shorter side of panel: s (in)",min_value=0.0001)
+        l = st.number_input("Longer side of panel: l (in)",min_value=s)
+        alpha = s/l
+        st.write(f'Aspect ratio {alpha = }')
+
+    stress_inputs = st.expander("Stress Data (Refer Figure 3)",expanded=True)
+    with stress_inputs: 
+        st.markdown("## Load Data (Stresses)")
+        sigma_ax = st.number_input("Axial stress normal to shorter side (s): Sigma_ax (psi)",min_value=1e-6)
+        sigma_ay = st.number_input("Axial stress normal to longer side (l): Sigma_ay (psi)",min_value=1e-6)
+        sigma_bx = st.number_input("Bending stress normal to shorter side (s): Sigma_bx (psi)",min_value=1e-6)
+        sigma_by = st.number_input("Bending stress normal to longer side (l): Sigma_by (psi)",min_value=1e-6)
+        tau = st.number_input("Shear stress: tau (psi)",min_value=1e-6)
+        sigma_xmax = sigma_ax + sigma_bx
+        sigma_xmin = sigma_ax - sigma_bx
+        sigma_ymax = sigma_ay + sigma_by
+        sigma_ymin = sigma_ay - sigma_by
+        st.write(f'{sigma_xmax = }')
+        st.write(f'{sigma_xmin = }')
+        st.write(f'{sigma_ymax = }')
+        st.write(f'{sigma_ymin = }')
+        st.write(f'{tau = }')
+        
+    stiffener_inputs = st.expander("Stiffener Data (Refer Figure 2)",expanded=True)
+    with stiffener_inputs:
+        st.write("In Progress")
