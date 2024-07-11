3e6ce6ba649c8b2e072d147eb7116dca07cfabc8
amolnwagh <waghamoln02@gmail.com>
2024-07-11 15:45:59 +0530

    in progress

--- calculations/ABS_Plate_Buckling.py
+++ calculations/ABS_Plate_Buckling.py
@@ -9,35 +9,39 @@ class StiffenerType(Enum):
     FLAT_BAR = 3
     BULB_PLATE = 4
     PLATE_ELEM = 5
-    LOCAL_PL_OF_STIFFENERS = 6
+    WEB_PL_OF_STIFFENERS = 6
     LOCAL_PL_OF_CORRUGATED_PANELS = 7
+    
+class LoadCaseType(Enum):
+    NORMAL_OPERATION = 1
+    SEVERE_STORM = 2
 
 @dataclass
 class Panel:
     
     """
-    stiffener_type: 
+    stiffener_type: StiffenerType # Enumeration from Class StiffenerType
     s: float # length of shorter side of the plate panel (cm)
     l: float # length of longer side of the plate panel (cm)
     t: float # thickness of plating (cm)
+    sigma_ax: float # axial stress normal to shorter side (N/cm^2)
+    sigma_ay: float # axial stress normal to longer side (N/cm^2)
+    sigma_bx: float # bending stress normal to shorter side (N/cm^2)
+    sigma_by: float # bending stress normal to longer side (N/cm^2)
     sigma_0: float  = 235000 # yeild stress of panel material (N/cm^2)
-    sigma_ax: float = 1e-6 # axial stress normal to shorter side (N/cm^2)
-    sigma_ay: float = 1e-6 # axial stress normal to longer side (N/cm^2)
-    sigma_bx: float = 1e-6 # bending stress normal to shorter side (N/cm^2)
-    sigma_by: float = 1e-6 # bending stress normal to longer side (N/cm^2)
     tau: float = 1e-6 # edge shear stress (N/cm^2)
     E: float = 2.06e7 # modulus of elasticity (N/cm^2)
     nu: float = 0.3 # poisson's ratio for steel
     """
-    stiffener_type: StiffenerType #
+    stiffener_type: StiffenerType # Enumeration from Class StiffenerType
     s: float # length of shorter side of the plate panel (cm)
     l: float # length of longer side of the plate panel (cm)
     t: float # thickness of plating (cm)
+    sigma_ax: float # axial stress normal to shorter side (N/cm^2)
+    sigma_ay: float # axial stress normal to longer side (N/cm^2)
+    sigma_bx: float # bending stress normal to shorter side (N/cm^2)
+    sigma_by: float # bending stress normal to longer side (N/cm^2)
     sigma_0: float  = 235000 # yeild stress of panel material (N/cm^2)
-    sigma_ax: float = 1e-6 # axial stress normal to shorter side (N/cm^2)
-    sigma_ay: float = 1e-6 # axial stress normal to longer side (N/cm^2)
-    sigma_bx: float = 1e-6 # bending stress normal to shorter side (N/cm^2)
-    sigma_by: float = 1e-6 # bending stress normal to longer side (N/cm^2)
     tau: float = 1e-6 # edge shear stress (N/cm^2)
     E: float = 2.06e7 # modulus of elasticity (N/cm^2)
     nu: float = 0.3 # poisson's ratio for steel
@@ -64,14 +68,14 @@ class Panel:
     def C1(self):
         return calc_C1(self.stiffener_type)
           
-    def k_s(self):
-        return calc_k_s(self.Alpha(), self.C1())
+    def k_s_tau(self):
+        return calc_k_s_tau(self.Alpha(), self.C1())
     
     def tau_E(self):
-        return calc_tau_E(self.k_s(),self.t, self.s, self.E, self.nu)
+        return calc_stress_E(self.k_s_tau(),self.t, self.s, self.E, self.nu)
     
     def tau_C(self):
-        return calc_tau_C(self.tau_0(), self.tau_E())
+        return calc_stress_C(self.tau_0(), self.tau_E())
         
     
 def calc_Alpha(l:float,s:float) -> float:
@@ -145,16 +149,25 @@ def calc_tau_0(sigma_0:float) -> float:
     return tau_0
 
 
-def calc_k_s(Alpha:float, C1:float) -> float:
+def calc_k_s_tau(Alpha:float, C1:float) -> float:
+    """calculates boundary dependent constant for shear buckling
+
+    Args:
+        Alpha (float): aspect ratio of the plate panel
+        C1 (float): value of C1 based on Stiffener type
+
+    Returns:
+        float: boundary dependent constant for shear buckling
+    """
     k_s = (4.0 * (1/Alpha)**2 + 5.34) * C1
     return k_s
 
   
-def calc_tau_E(k_s:float, t:float, s:float, E:float = 2.06e7, nu:float=0.3) -> float:
+def calc_stress_E(k_s_stress:float, t:float, s:float, E:float = 2.06e7, nu:float=0.3) -> float:
     """calculate elastic shear buckling stress
 
     Args:
-        k_s (float): boundary dependent constant
+        k_s_stress (float): boundary dependent constant
         t (float): thickness of plating, cm
         s (float): length of short plate edge, cm
         E (float, optional): modulus of elasticity. Defaults to 2.06e7, N/cm2 .
@@ -163,21 +176,116 @@ def calc_tau_E(k_s:float, t:float, s:float, E:float = 2.06e7, nu:float=0.3) -> f
     Returns:
         float: elastic shear buckling stress, N/cm2 
     """
-    tau_E = k_s * (((pi**2) * E)/(12 * (1 - (nu**2))) ) * (t/s)**2
-    return tau_E
+    stress_E = k_s_stress * (((pi**2) * E)/(12 * (1 - (nu**2))) ) * (t/s)**2
+    return stress_E
 
 
-def calc_tau_C(tau_0:float, tau_E:float, P_r:float = 0.6) -> float:
-    if tau_E <= P_r * tau_0:
-        tau_C = tau_E
-        return tau_C
+def calc_stress_C(stress_0:float, stress_E:float, P_r:float = 0.6) -> float:
+    """calculate critical buckling stress for edge shear
+    Args:
+        stress_0 (float): shear strength of plate, N/cm2
+        stress_E (float): elastic shear buckling stress, N/cm2
+        P_r (float, optional): proportional linear elastic limit of the structure. Defaults to 0.6 for steel.
+
+    Returns:
+        float: critical buckling stress for edge shear, N/cm2
+    """
+    if stress_E <= P_r * stress_0:
+        stress_C = stress_E
+        return stress_C
     else:
-        tau_C = tau_0 * (1 - P_r * (1 - P_r) * (tau_0 / tau_E))
-        return tau_C
+        stress_C = stress_0 * (1 - P_r * (1 - P_r) * (stress_0 / stress_E))
+        return stress_C
     
        
+def calc_eta(load_case_type: LoadCaseType) -> float:
+    """calculates maximum allowable strength factor
+
+    Args:
+        load_case_type (LoadCaseType): Enumeration from Class LoadCaseType
+
+    Returns:
+        float: maximum allowable strength factor, eta
+    """
+    phi = 1.0 # Adjustment factor ABS Buckling Requirements (WSD), Cl 3-1.7
+    if load_case_type == LoadCaseType.NORMAL_OPERATION:
+        eta = 0.6 * phi
+        return eta
+    else:
+        eta = 0.8 * phi
+        return eta
+
+    
+def calc_kappa(sigma_min:float, sigma_max:float) -> float:
+    """calculates ratio of edge stresses
+
+    Args:
+        sigma_min (float): minimum stress (Axial Stress + Bending Stress), N/cm2
+        sigma_max (float): maximum stress (Axial Stress - Bending Stress), N/cm2
+
+    Returns:
+        float: ratio of edge stresses, kappa
+    """
+    try:
+        kappa = sigma_min/sigma_max
+        return kappa
+    except: ValueError
+    
+    
+def calc_C2(stiffener_type: StiffenerType) -> float:
+    """calculates value of C2 based on Stiffener type
+
+    Args:
+        stiffener_type (StiffenerType): Enumeration from Class StiffenerType
+        
+    Returns:
+        float: value of C2   
+    """
+    if stiffener_type in [StiffenerType.ANGLE, StiffenerType.TEE]:
+        C2 = 1.2
+        return C2
+    elif stiffener_type in [StiffenerType.FLAT_BAR, StiffenerType.BULB_PLATE]:
+        C2 = 1.1
+        return C2
+    else:
+        C2 = 1.0
+        return C2
     
 
+def calc_k_s_sigma_x(C1:float, kappa_x:float) -> float:
+    """calculates boundary dependant factor for stress sigma_x (normal to shorter side)
+    
+    Args:
+        C1 (float): value of C1 based on Stiffener type
+        kappa_x (float): boundary dependant factor for stress sigma_x (normal to shorter side)
+
+    Returns:
+        float: boundary dependant factor for stress normal to shorter side
+    """
+    if 0 <= kappa_x <= 1.0:
+        k_s_sigma_x = C1 * (8.4/(kappa_x + 1.1))
+    elif -1.0 <= kappa_x < 0.0:
+        k_s_sigma_x = C1 * (7.6 - (6.4 * kappa_x) + 10*(kappa_x**2))
+    return k_s_sigma_x
 
 
+def calc_k_s_sigma_y(C2:float, Alpha:float, kappa_y:float) -> float:
+    """calculates boundary dependant factor for stress sigma_y (normal to longer side)
+    
+    Args:
+        C2 (float): value of C2 based on Stiffener type
+        Alpha (float): aspect ratio of the plate panel
+        kappa_y (float): _description_
+
+    Returns:
+        float: boundary dependant factor for stress sigma_y (normal to longer side)
+    """
+    if kappa_y < (1/3):
+        if 1.0 <= Alpha <= 2.0:
+            k_s_sigma_y = C2 * (1.0875 * (1 + (1/(Alpha**2)))**2 - (18/(Alpha**2))) * (1 + kappa_y) + (24/(Alpha**2))
+        elif Alpha > 2.0:
+            k_s_sigma_y = C2 * (1.0875 * (1 + (1/(Alpha**2)))**2 - (9/(Alpha**2))) * (1 + kappa_y) + (12/(Alpha**2))
+    elif kappa_y >= (1/3):
+        k_s_sigma_y = C2 * (1 + (1/(Alpha**2)))**2 * (1.675 - (0.675*kappa_y))
+    return k_s_sigma_y
 
--- calculations/test_ABS_Plate_Buckling.py
+++ calculations/test_ABS_Plate_Buckling.py
@@ -22,29 +22,53 @@ def test_calc_C1():
     assert math.isclose(ABS.calc_C1(ABS.StiffenerType.FLAT_BAR),1)
     assert math.isclose(ABS.calc_C1(ABS.StiffenerType.BULB_PLATE),1)
     assert math.isclose(ABS.calc_C1(ABS.StiffenerType.PLATE_ELEM),1)
-    assert math.isclose(ABS.calc_C1(ABS.StiffenerType.LOCAL_PL_OF_STIFFENERS),1)
-    assert math.isclose(ABS.calc_C1(ABS.StiffenerType.LOCAL_PL_OF_CORRUGATED_PANELS),1)     
+    assert math.isclose(ABS.calc_C1(ABS.StiffenerType.WEB_PL_OF_STIFFENERS),1)
+    assert math.isclose(ABS.calc_C1(ABS.StiffenerType.LOCAL_PL_OF_CORRUGATED_PANELS),1)
+    
+def test_calc_C2():
+    assert math.isclose(ABS.calc_C2(ABS.StiffenerType.ANGLE),1.2)
+    assert math.isclose(ABS.calc_C2(ABS.StiffenerType.TEE),1.2)
+    assert math.isclose(ABS.calc_C2(ABS.StiffenerType.FLAT_BAR),1.1)
+    assert math.isclose(ABS.calc_C2(ABS.StiffenerType.BULB_PLATE),1.1)
+    assert math.isclose(ABS.calc_C2(ABS.StiffenerType.PLATE_ELEM),1)
+    assert math.isclose(ABS.calc_C2(ABS.StiffenerType.WEB_PL_OF_STIFFENERS),1)
+    assert math.isclose(ABS.calc_C2(ABS.StiffenerType.LOCAL_PL_OF_CORRUGATED_PANELS),1)         
     
 
 def test_calc_tau_0():
     assert math.isclose(ABS.calc_tau_0(23500), 13567.73133)
 
 
-def test_calc_k_s():
-    assert math.isclose(ABS.calc_k_s(1, 1), 9.34)
-    assert math.isclose(ABS.calc_k_s(1, 1.1), 10.274)
-    assert math.isclose(ABS.calc_k_s(2, 1), 6.34)
-    assert math.isclose(ABS.calc_k_s(2, 1.1), 6.974)
+def test_calc_k_s_tau():
+    assert math.isclose(ABS.calc_k_s_tau(1, 1), 9.34)
+    assert math.isclose(ABS.calc_k_s_tau(1, 1.1), 10.274)
+    assert math.isclose(ABS.calc_k_s_tau(2, 1), 6.34)
+    assert math.isclose(ABS.calc_k_s_tau(2, 1.1), 6.974)
     
  
-def test_calc_tau_E():
-    assert math.isclose(ABS.calc_tau_E(k_s=10.274, t=1.2, s=60), 76514.52387)
+def test_calc_stress_E():
+    assert math.isclose(ABS.calc_stress_E(k_s_stress=10.274, t=1.2, s=60), 76514.52387)
 
 
-def test_calc_tau_C():
-    assert math.isclose(ABS.calc_tau_C(tau_0=10000, tau_E=5000),5000.0)
-    assert math.isclose(ABS.calc_tau_C(tau_0=10000, tau_E=6000),6000.0) 
-    assert math.isclose(ABS.calc_tau_C(tau_0=10000, tau_E=7000),6571.428571)
+def test_calc_stress_C():
+    assert math.isclose(ABS.calc_stress_C(stress_0=10000, stress_E=5000),5000.0)
+    assert math.isclose(ABS.calc_stress_C(stress_0=10000, stress_E=6000),6000.0) 
+    assert math.isclose(ABS.calc_stress_C(stress_0=10000, stress_E=7000),6571.428571)
+    
+       
+def test_calc_eta():
+    assert math.isclose(ABS.calc_eta(ABS.LoadCaseType.NORMAL_OPERATION),0.6)
+    assert math.isclose(ABS.calc_eta(ABS.LoadCaseType.SEVERE_STORM),0.8)
+    
+
+def test_calc_kappa():
+    assert ABS.calc_kappa(ABS.calc_sigma_min(0,10),ABS.calc_sigma_max(0,10)) == -1.0
+    assert ABS.calc_kappa(ABS.calc_sigma_min(100,0),ABS.calc_sigma_max(100,0)) == 1.0
+    assert math.isclose(ABS.calc_kappa(ABS.calc_sigma_min(100,10),ABS.calc_sigma_max(100,10)),0.8181818182)
+    assert math.isclose(ABS.calc_kappa(ABS.calc_sigma_min(10,100),ABS.calc_sigma_max(10,100)),-0.8181818182)
+    assert 1.0 > ABS.calc_kappa(ABS.calc_sigma_min(100,10),ABS.calc_sigma_max(100,10)) > 0.0
+    assert 0.0 > ABS.calc_kappa(ABS.calc_sigma_min(10,100),ABS.calc_sigma_max(10,100)) > -1.0
+    
     
 my_panel = ABS.Panel(
     stiffener_type=ABS.StiffenerType.ANGLE,
@@ -88,8 +112,8 @@ def test_Panel_C1():
     assert math.isclose(my_panel.C1(),1.1)
     
         
-def test_Panel_k_s():
-    assert math.isclose(my_panel.k_s(),6.974)
+def test_Panel_k_s_tau():
+    assert math.isclose(my_panel.k_s_tau(),6.974)
     
 
 def test_Panel_tau_E():
