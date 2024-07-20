4b8b402c6a49dc8a8f46ac8e50ebf84b714c5761
amolnwagh <waghamoln02@gmail.com>
2024-07-20 18:10:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from plotly.validators.scatter.marker import SymbolValidator
 
 st.markdown("# ABS Plate Buckling Checks")
 st.markdown("### (WSD Method) - *July 2022 Edition*")
+* Link to ABS Buckling Requirements: https://pub-rm20.apps.eagle.org/r/4/2022-07-01/Buckling-and-Ultimate-Strength-Assessment-for-Offshore-Structures
+
+* This app calculates the buckling state limit of a stiffened plated panel as per Section 3 of the ABS reference as mentioned above.
+* Such stiffened panels are typically used in hulls of ships and floating offshore structures like jack-up rigs and semi-submersibles.
+* Generally, the inputs are details regarding plate panel dimensions and material properties.
+* Instead of loads, the inputs are stresses, since these buckling check calculations are used typically after extracting stresses from a detailed finite element analysis model of the mentioned structures.
+* Currently, the app gives buckling state limit (UC) check if you have entered the stresses.
+* Even if the stresses are not input, the critical buckling state limits are calculated.
+* Additionally, interactive plotly charts are presented for the values of the critical buckling stresses and UCs (if stresses are supplied) for the defined panel and also for a range of aspect ratios of the panel, keeping all other inputs same.
 st.divider()
 
 st.image(r"figures/typ_stiffened_panel.png","Figure 1: Typical Stiffened Panel")
